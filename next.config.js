abaed1cf373900e292fb0dbf643aa0622afa4968
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-02 12:33:33 +0800

    all

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-// const withPWA = require('next-pwa')({
-// 	dest: 'public',
-// 	register: true,
-// 	skipWaiting: true,
-// })
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})
 
-// module.exports = withPWA({
-// 	reactStrictMode: true,
-// })
-
-
-// @ts-nocheck
-const withPlugins = require('next-compose-plugins')
-const withPWA = require('next-pwa')
-
-const nextConfig = {
-	reactStrictMode: true,
-}
-
-module.exports = withPlugins([withPWA], nextConfig)
+module.exports = {...withPWA({ reactStrictMode: true })}
